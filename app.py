64dd3fc3453cb31ddd40cfad93f589dcb68e5a69
Mohd-Dilshan <mohddilshaninvertian@gmail.com>
2026-02-01 22:06:03 +0530

    Update the contact information

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-import os
+# import os
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
+
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 10000))
+#     app.run(host="0.0.0.0", port=port)
+
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
+
+
